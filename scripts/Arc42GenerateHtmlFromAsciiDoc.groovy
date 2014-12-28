4ed78509c294d7348300e405608ac90c42bcc311
Ralf D. Mueller <ralf.d.mueller@gmail.com>
2014-12-28 23:37:58 +0100

    open issues are now exported from github into docs

--- scripts/Arc42GenerateHtmlFromAsciiDoc.groovy
+++ scripts/Arc42GenerateHtmlFromAsciiDoc.groovy
@@ -2,9 +2,31 @@
     @Grab('org.asciidoctor:asciidoctor-java-integration:0.1.4'),
 ])
 import org.asciidoctor.*
+import org.codehaus.groovy.grails.plugins.GrailsPluginUtils
+import grails.converters.*
+
+def pluginDir = GrailsPluginUtils.pluginInfos.find { it.name == "film-strip" }.pluginDir
+
+target(githubIssues2AsciiDoc: "fetch github issues and render them as asciidoc") {
+    
+    def issues = JSON.parse(new URL('https://api.github.com/repos/rdmueller/grails-filmStrip/issues?state=open').text)
+    new File('src/doc/generated').mkdirs()
+    def out = new File('src/doc/generated/issues.adoc')
+    out.write('''
+[options="header",cols="1,1,10"]
+|====
+|ID    |State  | Description
+''')
+    issues.each { issue ->
+        out.append("|${issue.url}[FS-${issue.number}] | ${issue.state} | ${issue.title}\n")
+    }
+    out.append('''|====
+''')
+}
 
 target(generateHtml: "compile AsciiDoc to HTML") {
-
+    githubIssues2AsciiDoc()
+    
     def asciidoctor = Asciidoctor.Factory.create()
                       
     def output = asciidoctor.renderFile(
@@ -24,6 +46,7 @@ target(generateHtml: "compile AsciiDoc to HTML") {
                             )
     new File('target/doc/').mkdirs()
     new File('target/doc/architecture.html').write(output,'utf-8')									
+    new File('../grails-filmStrip.io/index.html').write(output,'utf-8')									
     println "FilmStrip: finished generating docu at target/doc/architecture.html"									
 }
 
